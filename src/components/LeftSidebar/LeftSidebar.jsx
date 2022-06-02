566ae9299018a64938fcc69173425c519a106262
AnjaliDeshwani <deshwanianjali@gmail.com>
2022-06-02 17:08:24 +0530

    added Link on logo

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 export const LeftSidebar = () => {
   const isActiveLink = ({ isActive }) => {
     return `flex gap-3 cursor-pointer sm:hover:bg-gray-400 sm:hover:bg-opacity-30 py-1 px-3 lg:py-2 lg:px-5 rounded-full ${
@@ -7,9 +7,12 @@ export const LeftSidebar = () => {
   };
   return (
     <div className="sm:col-span-1 lg:col-span-2 flex justify-center">
-      <div className="lg:fixed pb-4 mt-4 font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-sky-500 to-cyan-400 -tracking-wide italic">
+      <Link
+        to="/"
+        className="lg:fixed pb-4 mt-4 font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-sky-500 to-cyan-400 -tracking-wide italic"
+      >
         Vibegram
-      </div>
+      </Link>
       <nav className="fixed z-10 w-full sm:w-auto bg-gray-100 sm:bg-transparent border-t-2 sm:border-t-0 border-t-slate-300 bottom-0 sm:bottom-auto flex flex-row justify-between sm:flex-col  gap-10 sm:gap-6 text-xl mt-16">
         <NavLink to="/" className={isActiveLink}>
           <span>
